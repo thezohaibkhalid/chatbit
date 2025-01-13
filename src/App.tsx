5f1c4c76d6ebb597ebe38cce4f95c0571bb5fd67
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-01-13 23:40:51 +0500

    initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="grid grid-cols-4 gap-4 mx-[100px] my-[50px] backdrop-blur-lg">
+        <div className="col-span-1">
+          chats
+        </div>
+
+        <div className="col-span-2">
+          chatting
+        </div>
+
+        <div className="col-span-1">
+          details
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+const style = {
+  backgroundImage: "url('https://plus.unsplash.com/premium_photo-1700670283974-712d61ab372b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+  backgroundSize: "cover", // Ensures the image covers the entire div
+  backgroundPosition: "center", // Centers the background image
+  height: "100vh", // Makes sure the div takes full viewport height
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div  style={style}>
+      <App />
+    </div>
+  </StrictMode>
+);
