622c0d16b1c87302a9aaf67748a2969b3588ca95
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-01-20 08:21:09 +0500

    false updates

--- src/components/details/chatSettings.tsx
+++ src/components/details/chatSettings.tsx
@@ -4,6 +4,7 @@ import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 const chatSettings = () => {
   return (
     <div>
+    Another comonorenfoeirn
       <div className="flex py-8 px-4 justify-between">
                   <p>Shared Photos</p>
                   <div className="flex p-2 rounded-full bg-black/20 items-center justify-center">
